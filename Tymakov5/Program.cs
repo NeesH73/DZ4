d9bcf8eea3ed7ac7fbade320ff376b02f8e62a53
smetanin73i@gmail.com <smetanin73i@gmail.com>
2023-10-05 16:13:21 +0300

    Добавьте файлы проекта.

---
+++ Tymakov5/Program.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Tymakov5
+{
+    internal class Program
+    {
+        static int Max(int FN, int SN)
+        {
+            if (FN > SN)
+            {
+                return FN;
+            }
+            else
+            {
+                return SN;
+            }
+        }
+
+        static void Mesta(int ch1, int ch2)
+        {
+            int prom = ch1;
+            ch1 = ch2;
+            ch2 = prom;
+            Console.WriteLine($"Числа после обмена: {ch1}, {ch2}");
+        }
+        static bool Factorial(out int fact, int number)
+        {
+            try
+            {
+                fact = 1;
+
+                for (int i = 1; i <= number; i++)
+                {
+                    checked
+                    {
+                        fact *= i;
+                    }
+                }
+                return true;
+            }
+            catch (OverflowException)
+            {
+                fact = 0;
+                return false;
+            }
+        }
+        static int recFactorial(int num)
+        {
+            if (num == 1 || num==0)
+            {
+                return 1;
+            }
+            else
+            {
+                return num * recFactorial(num - 1);
+            }
+        }
+        static int Nod(int a, int b)
+        {
+            int nd = 0;
+            while(a!=b)
+            {
+                if(a>b)
+                {
+                    a = a - b;
+                }
+                else
+                {
+                    b = b - a;
+                }
+            }
+            nd = b;
+            return nd;
+        }
+        static int Nod(int a,int b,int c)
+        {
+            if (a!=0 &&  b!=0 && c!=0)
+            {
+                return Nod(Nod(a, b), c);
+            }
+            else
+            {
+                return a + b + c;
+            }
+        }
+        static int Fib(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return n;
+            }
+            return Fib(n - 1) + Fib(n - 2);
+        }
+
+            static void Main(string[] args)
+        {
+            Console.WriteLine("Упр 5.1: Вывести наиб.\n Введите два числа: ");
+            if (int.TryParse(Console.ReadLine(),out int FN) && int.TryParse(Console.ReadLine(),out int SN))
+            {
+                Console.WriteLine($"Максимальное число из введенных: {Max(FN, SN)}");
+            }
+            else
+            {
+                Console.WriteLine("Данные введены некорректно.");
+            }
+
+
+
+            Console.WriteLine("Упражнение 5.2: Поменять местами\n Введите два числа: ");
+            if (int.TryParse(Console.ReadLine(), out int ch1) && int.TryParse(Console.ReadLine(), out int ch2))
+            {
+                Console.WriteLine($"Числа до обмена: {ch1}, {ch2}");
+                Mesta(ch1,ch2);
+            }
+            else
+            {
+                Console.WriteLine("Данные введены некорректно");
+            }
+
+
+            Console.WriteLine("Упражнение 5.3: Метод для вычисления факториала\n Введите число, факториал которого нужно найти: ");
+            if (int.TryParse(Console.ReadLine(), out int number))
+            {
+                if (Factorial(out int fact, number))
+                {
+                    Console.WriteLine($"Факториал числа {number} равен {fact}");
+                }
+                else
+                {
+                    Console.WriteLine("Введите значение поменьше");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Данные введены некорректно.");
+            }
+
+
+
+            Console.WriteLine("Упраждение 5.4:рекурсивный метод для вычисления факториала\nВведите число для которого нужно найти факториал: ");
+            if (int.TryParse(Console.ReadLine(), out int num) && recFactorial(num) != 0)
+            {
+                Console.WriteLine($"Факториал числа {num} равен {recFactorial(num)}");
+            }
+            else if (recFactorial(num) == 0)
+            {
+                Console.WriteLine("Введите числовое значение поменьше");
+            }
+            else
+            {
+                Console.WriteLine("Введите числовое значение");
+            }
+
+
+
+
+            Console.WriteLine("Домашнее задание 5.1:метод НОД \nВведите три числовых значения");
+            if (int.TryParse(Console.ReadLine(), out int a) && int.TryParse(Console.ReadLine(), out int b) && int.TryParse(Console.ReadLine(), out int c))
+            {
+                Console.WriteLine($"Наибольший общий делитель для чисел {a}, {b} и {c} равен {Nod(a,b,c)}");
+                Console.WriteLine($"Наибольший общий делитель для чисел {a} и {b} равен {Nod(a,b)}");
+                
+            }
+            else
+            {
+                Console.WriteLine("Введите числовые значения");
+            }
+
+
+
+            Console.WriteLine("Домашнее задание 5.2: метод для чисел Фибаначи \nВведите номер n числа ряда Фибоначчи: ");
+            if (int.TryParse(Console.ReadLine(), out int n))
+            {
+                Console.WriteLine($"Номеру {n} соотвествует значение {Fib(n)}");
+            }
+            else
+            {
+                Console.WriteLine("Введите числовое значение");
+            }
+        }
+    }
+}
---
+++ ZADACHI/Program.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Threading;
+
+namespace ZADACHI
+{
+    internal class Program
+    {
+        static bool Perch(int[] array, int ch)
+        {
+            foreach(int i in array)
+            {
+                if (i==ch)
+                { 
+                    return true; 
+                }
+            }
+            return false;
+        }
+        static int slow(ref int pr, out double arif, params int[] array)
+        {
+            int summ = 0;
+
+            foreach (int number in array)
+            {
+                summ += number;
+                pr *= number;
+            }
+
+            arif = (double)summ / array.Length;
+
+            return summ;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задача 1: Поменять в массиве из 20 чисел 2 числа");
+            int[] array = new int[20];
+            Random rnd = new Random();
+            for (int i = 0; i<array.Length;i++)
+            {
+                array[i] = rnd.Next();
+            }
+            if (int.TryParse(Console.ReadLine(), out int ch1) && int.TryParse(Console.ReadLine(), out int ch2))
+            {
+                if (Perch(array, ch1) && Perch(array, ch2))
+                {
+                    (array[ch1], array[ch2]) = (array[ch2], array[ch1]);
+                    Console.WriteLine(array);
+                }
+                else
+                {
+                    Console.WriteLine("Таких чисел нет в массиве");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Введите числа");
+            }
+
+
+            Console.WriteLine("Задача 2: Написать метод для произведения, суммы, ср.ариф. ");
+            int summ;
+            double arif;
+            int pr = 1;
+            summ = slow(ref pr, out arif, array);
+            Console.WriteLine($"Сумма - {summ}, произведение -{pr} , ср. ариф. - {arif}");
+
+
+            Console.WriteLine("Задача 3: Написать цифры с помощью #");
+            try
+            {
+                if (int.TryParse(Console.ReadLine(), out int chislo))
+                {
+                    switch (chislo)
+                    {
+                        case 0:
+                            Console.WriteLine("\t#####\n" +
+                                              "\t#   #\n" +
+                                              "\t#   #\n" +
+                                              "\t#   #\n" +
+                                              "\t#   #\n" +
+                                              "\t#   #\n" +
+                                              "\t#####\n");
+                            break;
+                        case 1:
+                            Console.WriteLine("\t    #\n" +
+                                              "\t  # #\n" +
+                                              "\t #  #\n" +
+                                              "\t    #\n" +
+                                              "\t    #\n" +
+                                              "\t    #\n" +
+                                              "\t    #\n");
+                            break;
+                        case 2:
+                            Console.WriteLine("\t#####\n" +
+                                              "\t    #\n" +
+                                              "\t    #\n" +
+                                              "\t#####\n" +
+                                              "\t#    \n" +
+                                              "\t#    \n" +
+                                              "\t#####\n");
+                            break;
+                        case 3:
+                            Console.WriteLine("\t#####\n" +
+                                              "\t    #\n" +
+                                              "\t    #\n" +
+                                              "\t#####\n" +
+                                              "\t    #\n" +
+                                              "\t    #\n" +
+                                              "\t#####\n");
+                            break;
+                        case 4:
+                            Console.WriteLine("\t#   #\n" +
+                                              "\t#   #\n" +
+                                              "\t#   #\n" +
+                                              "\t#####\n" +
+                                              "\t    #\n" +
+                                              "\t    #\n" +
+                                              "\t    #\n");
+                            break;
+                        case 5:
+                            Console.WriteLine("\t#####\n" +
+                                              "\t#    \n" +
+                                              "\t#    \n" +
+                                              "\t#####\n" +
+                                              "\t    #\n" +
+                                              "\t    #\n" +
+                                              "\t#####\n");
+                            break;
+                        case 6:
+                            Console.WriteLine("\t#####\n" +
+                                              "\t#    \n" +
+                                              "\t#    \n" +
+                                              "\t#####\n" +
+                                              "\t#   #\n" +
+                                              "\t#   #\n" +
+                                              "\t#####\n");
+                            break;
+                        case 7:
+                            Console.WriteLine("\t######\n" +
+                                              "\t#    #\n" +
+                                              "\t    #\n" +
+                                              "\t  #####\n" +
+                                              "\t    #\n" +
+                                              "\t    #\n" +
+                                              "\t    #\n");
+                            break;
+                        case 8:
+                            Console.WriteLine("\t#####\n" +
+                                              "\t#   #\n" +
+                                              "\t#   #\n" +
+                                              "\t#####\n" +
+                                              "\t#   #\n" +
+                                              "\t#   #\n" +
+                                              "\t#####\n");
+                            break;
+                        case 9:
+                            Console.WriteLine("\t#####\n" +
+                                              "\t#   #\n" +
+                                              "\t#   #\n" +
+                                              "\t#####\n" +
+                                              "\t    #\n" +
+                                              "\t    #\n" +
+                                              "\t#####\n");
+                            break;
+                        default:
+                            Console.BackgroundColor = ConsoleColor.Red;
+                            Console.WriteLine("\nВы ввели число меньше 0 или больше 9!\n");
+                            Thread.Sleep(3000);
+                            Console.ResetColor(); ;
+                            break;
+                    }
+                }
+                else
+                {
+                    throw new Exception("Введите число");
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+
+            }
+
+
+
+
+
+        }
+    }
+}
